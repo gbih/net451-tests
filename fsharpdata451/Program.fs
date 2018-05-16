1236c1f7a0dd925624b780a14319f36a0d79b6ae
George <george@omame.com>
2018-05-16 16:30:10 +0900

    first commit for net451 tests

---
+++ fsharpdata451/Program.fs
@@ -0,0 +1,13 @@
+﻿open FSharp.Data
+
+[<Literal>]
+let sample = "http://api.openweathermap.org/data/2.5/weather?APPID=96916a5177869f140f4f2b85d6df30ca&q=London"
+let apiUrl = "http://api.openweathermap.org/data/2.5/weather?APPID=96916a5177869f140f4f2b85d6df30ca&q="
+
+type Weather = JsonProvider<sample>
+
+let city = Weather.Load(apiUrl + "Tokyo")
+printfn "city.Sys.Country: %s" city.Sys.Country
+printfn "city.Wind.Speed: %f" city.Wind.Speed
+printfn "city.Main.Temp: %f" city.Main.Temp
+printfn "city.Clouds: %f" city.Wind.Deg
---
+++ suavedotliquid451/Program.fs
@@ -0,0 +1,39 @@
+﻿open DotLiquid
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.DotLiquid
+open FSharp.Data
+
+setTemplatesDir "./templates"
+
+[<Literal>]
+let sample = "http://api.openweathermap.org/data/2.5/weather?APPID=96916a5177869f140f4f2b85d6df30ca&q=London"
+
+let apiUrl = "http://api.openweathermap.org/data/2.5/weather?APPID=96916a5177869f140f4f2b85d6df30ca&q="
+
+type Weather = JsonProvider<sample>
+
+let city = Weather.Load(apiUrl + "Tokyo")
+
+type Model = {
+    name : string
+    windspeed : decimal
+    maintemp : decimal
+    humidity: int
+}
+
+let o = { 
+    name = city.Name
+    windspeed = city.Wind.Speed
+    maintemp = city.Main.Temp
+    humidity = city.Main.Humidity
+    }
+
+let app =
+  choose
+    [ GET >=> choose
+        [ path "/" >=> page "my_page.liquid" o ]]
+
+startWebServer defaultConfig app
